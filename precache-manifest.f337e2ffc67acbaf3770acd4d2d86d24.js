346e9b22084d7331ce47a4da9da1e4a051a28f22
Deepika <kulkdeepika@gmail.com>
2020-04-22 22:48:33 -0700

    Updates

---
+++ precache-manifest.f337e2ffc67acbaf3770acd4d2d86d24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac8fdc740505cb57bc379b492b05598",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd6fe49fab6cffdc57a7",
+    "url": "/static/css/main.bd884fe0.chunk.css"
+  },
+  {
+    "revision": "f83bcf3cc646f2d784d3",
+    "url": "/static/js/2.046a7d96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.046a7d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd6fe49fab6cffdc57a7",
+    "url": "/static/js/main.93668699.chunk.js"
+  },
+  {
+    "revision": "ffcbbf5fac557c70edad",
+    "url": "/static/js/runtime-main.3fa09e1f.js"
+  }
+]);
